3806f75f0678e839eff98915c8812de14671f0cb
Charlotte <chacptzy258@gmail.com>
2022-02-28 00:31:20 +0800

    write into a file and change the type of input data

--- products/main.py
+++ products/main.py
@@ -7,6 +7,7 @@ while True:
     #p=[]
     #p.append(name)
     #p.append(price)
+    price=int(price)
     p=[name,price]
     products.append(p)
 
@@ -14,3 +15,7 @@ print(products)
 
 for product in products:
     print(product[0],'的價格是',product[1])
+
+with open('products.csv','w') as f: #with的功能就是自動close
+    for p in products:
+        f.write(p[0]+','+str(p[1])+'\n') #因為+號是字串才能用所以要改成字串型態
