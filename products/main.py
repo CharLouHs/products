9c1513e0ffd7e77c79a4709648e60de5a4c004af
Charlotte <chacptzy258@gmail.com>
2022-02-28 16:23:38 +0800

    check if the file is exist and check title

--- products/main.py
+++ products/main.py
@@ -1,10 +1,18 @@
+import os
 #讀取檔案
 products=[]
-with open('products.csv','r',encoding='utf-8') as f:
-    for line in f:
-        name,price=line.strip().split(',') #將換行符號和前後空格去掉(split)
-        products.append([name,price])
-print(products)
+if os.path.isfile('products.csv'): #os的file裡面的isfile功能 檢查是否有檔案
+    print('yeah! 找到檔案了!!')
+    with open('products.csv', 'r', encoding='utf-8') as f:
+        for line in f:
+            name, price = line.strip().split(',')  # 將換行符號和前後空格去掉(split)
+            products.append([name, price])
+    print(products)
+else:
+    print('找不到檔案。。。')
+
+
+
 #讓使用者輸入
 while True:
     name=input('please input the name of product')
@@ -25,5 +33,7 @@ for product in products:
 #寫入檔案
 with open('products.csv','w',encoding='utf-8') as f: #with的功能就是自動close
     #f.write('商品,價格\n')
+    if products[0] != ['商品','價格'] :
+        f.write('商品,價格\n')
     for p in products:
         f.write(p[0]+','+str(p[1])+'\n') #因為+號是字串才能用所以要改成字串型態
