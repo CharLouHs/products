3c4f6709c618bc7999467ec3ff912a4060b8275e
Charlotte <chacptzy258@gmail.com>
2022-02-28 15:49:57 +0800

    add reading the file

--- products/main.py
+++ products/main.py
@@ -1,4 +1,11 @@
+#讀取檔案
 products=[]
+with open('products.csv','r',encoding='utf-8') as f:
+    for line in f:
+        name,price=line.strip().split(',') #將換行符號和前後空格去掉(split)
+        products.append([name,price])
+print(products)
+#讓使用者輸入
 while True:
     name=input('please input the name of product')
     if name== 'q':
@@ -12,11 +19,11 @@ while True:
     products.append(p)
 
 print(products)
-
+#印出所有購買紀錄
 for product in products:
     print(product[0],'的價格是',product[1])
-
+#寫入檔案
 with open('products.csv','w',encoding='utf-8') as f: #with的功能就是自動close
-    f.write('商品,價格\n')
+    #f.write('商品,價格\n')
     for p in products:
         f.write(p[0]+','+str(p[1])+'\n') #因為+號是字串才能用所以要改成字串型態
