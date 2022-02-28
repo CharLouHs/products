7082719d33d0c0e3041213f259be5c5b3e37be37
Charlotte <chacptzy258@gmail.com>
2022-02-28 15:25:34 +0800

    add title and utf-8

--- products/main.py
+++ products/main.py
@@ -16,6 +16,7 @@ print(products)
 for product in products:
     print(product[0],'的價格是',product[1])
 
-with open('products.csv','w') as f: #with的功能就是自動close
+with open('products.csv','w',encoding='utf-8') as f: #with的功能就是自動close
+    f.write('商品,價格\n')
     for p in products:
         f.write(p[0]+','+str(p[1])+'\n') #因為+號是字串才能用所以要改成字串型態
